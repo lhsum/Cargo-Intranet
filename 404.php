031c632359ed0fc14b15c501d049b4497c9f3aa9
Henry Leung <lp128henry@gmail.com>
2013-08-29 15:59:44 +0800

    Hide Search function on 404 page

--- 404.php
+++ 404.php
@@ -8,8 +8,8 @@
         <?php echo '<h2>' . __('Page Not Found', 'theme1843') . '</h2>'; ?>
       </hgroup>
       <?php echo '<h4>' . __('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'theme1843') . '</h4>'; ?>
-      <?php echo '<p>' . __('Please try using our search box below to look for information on the internet.', 'theme1843') . '</p>'; ?>
-      <?php get_search_form(); /* outputs the default Wordpress search form */ ?>
+      <?php //echo '<p>' . __('Please try using our search box below to look for information on the internet.', 'theme1843') . '</p>'; ?>
+      <?php //get_search_form(); /* hide search outputs the default Wordpress search form */ ?>
     </div>
 	</div><!--#error404 .post-->
 </div><!--#content-->
