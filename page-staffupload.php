568f0e33339eabbafff65646df5dbf79726e233a
Henry Leung <lp128henry@gmail.com>
2013-09-23 17:17:59 +0800

    Change Side Bar, Chung change staff upload

--- page-staffupload.php
+++ page-staffupload.php
@@ -1,151 +1,83 @@
 ﻿<?php
 get_header(); 
 
-/**
- * Template Name: Staff Upload
- */
-error_reporting (E_ALL ^ E_NOTICE);
-
-$searchName = $_POST["search_name"];
-$department = $_POST["department"];
-
 $dbh = new PDO('mysql:host=localhost;port=3306;dbname=staff;charset=utf8', 'root', '', array( PDO::ATTR_PERSISTENT => false));
+$message = isset($_GET["message"])?$_GET["message"]:"";
+?>
 
-		
-  ?>
-
-
-  
 <div class="<?php echo of_get_option('blog_sidebar_pos') ?>">
     <div id="content" class="grid_13 right <?php echo of_get_option('blog_sidebar_pos') ?>">
-
+		<!--#content-->
         <div class="header-title">
-    		<font class="location">	<img src="<?php echo $imgPath;?>/2013/07/icon_location_bar_arrow.gif" align="absmiddle">
-    		<?php if (qtrans_getLanguage() == 'en') {?>
-    		<a href="/intranet/">Home</a> >
-    		<?php }else{?>
-    		<a href="/intranet/?lang=zh">主页</a> >	
-    		<?php }?> <?php echo get_the_title(); ?>
-    		</font>
-			<br>
-			<!---- Search-staff--->
-			<!--<script src="/intranet/wp-content/themes/Cargo-Intranet/js/jquery-1.7.2.min.js"></script>-->
-			<script src="/intranet/wp-content/themes/Cargo-Intranet/js/jquery.form.js"></script> 
-			<script src="/intranet/wp-content/themes/Cargo-Intranet/js/waypoints.min.js"></script>
-			<script> 
-			// wait for the DOM to be loaded 
-			$(document).ready(function() { 
-				var searchFlag = false;
-				$('#simpleSearchForm').ajaxForm(function(data) {
-					
-					$('#searchResult').waypoint('destroy');
-					
-					var dataObject = jQuery.parseJSON(data);
-					
-					$('#searchInformation').find('#lblSearchKeyword').text(dataObject.searchKeywords);
-					$('#searchInformation').find('#lblRecordCount').text(dataObject.recordCount);
-					
-					if(parseInt(dataObject.recordCount) <= 0){
-						$('#searchInformation').hide();
-						$('#searchNoResult').show();
-						$('#searchResult').empty();
-					}else if(dataObject.haveMoreRecords){
-						if(parseInt(dataObject.from) == 0){
-							$('#searchResult').empty();
+        <script src="../js/jquery-1.10.1.min.js"></script>
+		<style type="text/css">
+		.button {
+			width:400px;
+			height:40px;
+			font-size:18px;
+			font-weight:bold;
+			font-family:Verdana;
+		}
+		.section *{
+			border:1px solid #CCCCCC;
+			padding:10px;
+			font-family:Trebuchet MS;
+		}
+		.section{
+			padding:5px;
+		}
+		h2{
+			margin:5px;
+			font-size:16px;
+			letter-spacing:0.07em;
+		}
+		.message{
+			color:red;
+			font-weight:bold;
+		}
+		.option{
+			margin-left:10px;
+		}
+		</style>
+
+		<?php
+			if($message != null && $message != ""){
+				?>
+					<h3 class="message"><?=$message?></h3>
+				<?php
+			}
+		?>
+
+		<h1>Staff update</h1>
+		<form name="UploadPage" method="post" enctype="multipart/form-data" action="../wp-content/themes/Cargo-Intranet/staff_update/preview_change.php"> 
+			<div class="section">
+				<h2>1) Select station/branch to update</h2>
+				<select name="branch" class="option">
+					<option value="">-- select station/branch --</option>
+					<?php
+						$stmt = $dbh->prepare("SELECT distinct * FROM company");
+						$stmt->execute();
+						while ($rs = $stmt->fetch(PDO::FETCH_OBJ)){
+							echo "<option value=\"$rs->company_id\">$rs->english_name</option>";
 						}
-						$('#searchInformation').show();
-						$('#searchNoResult').hide();
-						$('#searchResult').show();
-						$('#searchResult').append(dataObject.htmlCode);
-						$('#searchResult').waypoint(function() {
-							searchFlag = true;
-						}, { offset: 'bottom-in-view' });
-						
-						$('#group_' + dataObject.from).find('.lblGroup').html(dataObject.recordGroup);
-						
-					}
-				}); 
-				$('.searchTrigger').change(function(){
-					$('#simpleSearchForm').find('input[name="from"]').val(0);
-				});
-				setInterval(function(){
-					if(searchFlag){
-						$('#simpleSearchForm').find('input[name="from"]').val(parseInt($('#simpleSearchForm').find('input[name="from"]').val()) + 20);
-						$('#simpleSearchForm').submit();
-						searchFlag = false;
-					}
-				},250);
+					?>
+				</select> <br/>
 				
-				$('#simpleSearchForm').submit();
-			}); 
-			</script> 
-			<style type="text/css">
-			*{
-				font-family:Verdana, Geneva, sans-serif;
-				font-size: 12px;
-			}
-			td{
-				vertical-align:top;
-			}
-			.staff_search_result{
-				padding: 10px;
-				border: 2px;
-				border-color: #00b5c7;
-				-webkit-border-radius: 5px;
-				-moz-border-radius: 5px;
-				border-radius: 5px;
-				border-style: solid;
-				margin-bottom:4px;
-				margin-right: 14px;
-			}
-
-			</style>
-	<form method="post" action="/intranet/wp-content/themes/Cargo-Intranet/staff_search_ajax.php" id="simpleSearchForm">
-		<select name="department" style="margin:0px;padding:1px;" class="searchTrigger">
-		<option value="">-- Department --</option>
-			<?php
-			// call the stored procedure
-			$stmt = $dbh->prepare("SELECT DISTINCT department_id,department_name FROM department ORDER BY department_id ASC");
-			
-			$stmt->execute();
-
-			while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
-				print "<option value=\"" . $rs->department_id ."\"";
-				if($rs->department_id == $department){
-					print 'SELECTED="SELECTED"';
-				}
-				print ">";
-				print $rs->department_name;
-				print "</option>\n";
-			}
-			?>
-		</select>
-		<br/>
-		<input type="text" name="search_name" placeholder=" -- Name -- " style="width:200px" class="searchTrigger" value="<?=$searchName?>"/> 
-
-		<input type="submit" value="Search" />
-		<input type="hidden" name="from" value="0"/>
-	</form>
-	<br/>
-	<div id="searchInformation" style="display:none">
-		Search Keyword(s) : <span id="lblSearchKeyword"></span><br />
-
-		Total: <span id="lblRecordCount"></span> record(s) found.
-
-		</div>
-		<div id="searchNoResult" style="display:none">
-		<br/>
-		No record found. Please search again or try <a href="/intranet/advance-search/">Advance Search. </a>
+			</div>
+			<div class="section">
+				<h2>2) Select the excel File (*.xlsx)</h2>
+				<input type="file" name="staff_file" class="option" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"><br/> 
+			</div>
+			<div class="section">
+				<h2>3) Password : </h2><input type="password" name="password" class="option" style="width:150px"/>	
+			</div>
+			<div class="section">
+				<input type="submit" value="Upload >" class="button">
+			</div>
+		</form> 
+		<br /><a href="./export_excel.php">Export Excel</a>
+		
+        </div>
 	</div>
-	
-	<div id="searchResult">
-	
-	</div>
-	<br/>
-		</div>	
-			                  <!-- //.post-content -->
-	</div><!--#content-->
-			
-<?php get_sidebar(); ?>
 </div>			
 <?php get_footer(); ?>
--- staff_update/preview_change.php
+++ staff_update/preview_change.php
@@ -1,4 +1,7 @@
 <?php
+
+get_header(); 
+
 include 'ChromePhp.php';
 $fileName = "";
 
